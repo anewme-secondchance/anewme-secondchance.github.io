f8ddcace7a0ada92ebc3d46c4fbe5834356b9c20
anewme-secondchance <jonestadasha@gmail.com>
2026-02-18 17:48:33 -0600

    Create resources-data.js

---
+++ resources-data.js
@@ -0,0 +1,79 @@
+const resources = [
+  {
+    name: "PATH (People Attempting to Help)",
+    city: "Tyler",
+    category: "Housing",
+    phone: "9035977284",
+    website: "https://pathhelps.org/"
+  },
+  {
+    name: "Goodwill Industries of East Texas",
+    city: "Tyler",
+    category: "Employment",
+    phone: "9035938438",
+    website: "https://goodwilleasttexas.com/"
+  },
+  {
+    name: "Volunteers of America Texas",
+    city: "Dallas",
+    category: "Housing",
+    phone: "8175297300",
+    website: "https://www.voatx.org/"
+  },
+  {
+    name: "Miles of Freedom",
+    city: "Dallas",
+    category: "Employment",
+    phone: "",
+    website: "https://milesoffreedom.org/"
+  },
+  {
+    name: "Tarrant County Reentry Center",
+    city: "Fort Worth",
+    category: "Housing",
+    phone: "8176326000",
+    website: "https://www.tcreentry.org/"
+  },
+  {
+    name: "ACCESS Harris County",
+    city: "Houston",
+    category: "Housing",
+    phone: "",
+    website: "https://publichealth.harriscountytx.gov/"
+  },
+  {
+    name: "The Harris Center",
+    city: "Houston",
+    category: "Health",
+    phone: "7139707000",
+    website: "https://www.theharriscenter.org/"
+  },
+  {
+    name: "Bee Busy Wellness Center",
+    city: "Houston",
+    category: "Health",
+    phone: "7137712292",
+    website: "https://beebusy.org/"
+  },
+  {
+    name: "Bexar County Reentry Center",
+    city: "San Antonio",
+    category: "Housing",
+    phone: "2103356640",
+    website: "https://www.bexar.org/3509/Reentry-Center"
+  },
+  {
+    name: "Crosspoint, Inc.",
+    city: "San Antonio",
+    category: "Housing",
+    phone: "2102250864",
+    website: "https://crosspointinc.org/"
+  },
+  {
+    name: "Project RIO",
+    city: "Austin",
+    category: "Employment",
+    phone: "",
+    website: "https://www.texasworkforce.org/project-rio"
+  }
+];
